c07999023dbbd23869dc68a9870e69f74d01a4d7
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-07-18 06:40:34 -0700

    Practice 1

---
+++ src/Khahani.java
@@ -0,0 +1,10 @@
+public class Khahani {
+    public static void main(String[] args) {
+        // TODO: 1- create three rectangle
+        // TODO: 2- Print area of each rectangle
+        // TODO: 3- Print environment of each rectangle
+        // TODO: 4- Create two circle
+        // TODO: 5- Print area of each circle
+        // TODO: 6- Print environment of each circle
+    }
+}
