7134e6cce0665938c6d9d22e52eff0eb9e3bd3b2
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-07-18 06:53:56 -0700

    todo 2 - complete

--- src/Khahani.java
+++ src/Khahani.java
@@ -14,6 +14,14 @@ public class Khahani {
         height3 = 4;
 
         // TODO: 2- Print area of each rectangle
+        int area1 = width1 * height1;
+        System.out.println("Rectangle 1 area: " + area1);
+
+        System.out.println("Rectangle 2 area: " + width2 * height2);
+
+        System.out.println("Rectangle 3 area: " + width3 * height3);
+
+
         // TODO: 3- Print environment of each rectangle
         // TODO: 4- Create two circle
         // TODO: 5- Print area of each circle
