c3d5a378fd2403f77b9ed81325c534562959e6ec
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-07-18 06:53:40 -0700

    todo 1 - complete

--- src/Khahani.java
+++ src/Khahani.java
@@ -1,6 +1,18 @@
 public class Khahani {
     public static void main(String[] args) {
         // TODO: 1- create three rectangle
+        int width1, height1;
+        width1 = 2;
+        height1 = 4;
+
+        int width2, height2;
+        width2 = 5;
+        height2 = 3;
+
+        int width3, height3;
+        width3 = 10;
+        height3 = 4;
+
         // TODO: 2- Print area of each rectangle
         // TODO: 3- Print environment of each rectangle
         // TODO: 4- Create two circle
